7bb9495b6a56b379010630192aa67f982ed7d1c6
pqy <pqy@seas.upenn.edu>
2020-02-08 17:01:59 -0500

    edit get coord function

--- GoogleAPI.py
+++ GoogleAPI.py
@@ -11,11 +11,27 @@ def getDistance(origin, destination):
     response = urllib.request.urlopen(request).read()
     directions = json.loads(response)
     routes = directions['routes']
+    if len(routes)<0:
+        return "no valid route"
     legs = routes[0]['legs']
     return legs[0]['distance']['text']
 
+def getCoord(addr):
+    endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
+    apiKey = 'AIzaSyDZ3NzuWjs0Q-MB8xB7h4EzaSOFlQvIThQ'
+    addr = addr.replace(' ','+')
+    navRequest = 'origin={}&destination={}&key={}'.format(addr,addr,apiKey)
+    request = endpoint + navRequest
+    response = urllib.request.urlopen(request).read()
+    directions = json.loads(response)
+    routes = directions['routes']
+    if len(routes)<0:
+        return "no valid route"
+    legs = routes[0]['legs']
+    return [legs[0]['start_location']['lat'],legs[0]['start_location']['lng']]
 
 origin = "3411 Chestnut St, PA"
 destination = "3401 Grays Ferry Ave, PA"
 
 print(getDistance(origin, destination))
+print(getCoord(origin))
