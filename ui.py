2fb7b9c63d1ede815fb196aa60e5ed51d5701365
Estee (Yiwei) Chen <44720306+esteechenyiwei@users.noreply.github.com>
2020-02-08 19:35:59 -0500

    added good ones

--- ui.py
+++ ui.py
@@ -1,21 +1,29 @@
 from flask import *
 from flask_wtf import FlaskForm
 import json
+from flask_wtf.csrf import CSRFProtect
 
 from wtforms import StringField, TextField, SubmitField
 from wtforms.validators import DataRequired, Length, InputRequired
 
+import os
+SECRET_KEY = os.urandom(32)
+
+
 # from main import *
+
+from flask_wtf.csrf import CSRFProtect
 
 
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config.Config')
-
-
+csrf = CSRFProtect(app)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 class DonorForm(FlaskForm):
     """Contact form."""
+    csrf = True
     name = StringField('Your Organization Name', [
         InputRequired(), DataRequired()])
     address = StringField('Your Organization Address', [
@@ -54,8 +62,8 @@ class SeekerForm(FlaskForm):
 @app.route('/', methods=('GET', 'POST'))
 def index():
     donorForm = DonorForm()
-    if form.validate_on_submit():
-        return redirect(url_for('success'))
+
+    # return redirect(url_for('success'))
     # locList = returnLocList()
 
     return render_template('index.html', 
