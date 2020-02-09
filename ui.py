dc8576ec4fdabb8b47f94ef73167d4b337169898
Estee (Yiwei) Chen <44720306+esteechenyiwei@users.noreply.github.com>
2020-02-08 19:04:00 -0500

    add ui index style

--- ui.py
+++ ui.py
@@ -1,15 +1,17 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import *
 from flask_wtf import FlaskForm
+import json
+
 from wtforms import StringField, TextField, SubmitField
 from wtforms.validators import DataRequired, Length, InputRequired
-from back import *
+
+# from main import *
 
 
 
-app = Flask(__name__, instance_relative_config=False)
+app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config.Config')
-app.config['RECAPTCHA_PUBLIC_KEY'] = 'iubhiukfgjbkhfvgkdfm'
-app.config['RECAPTCHA_PARAMETERS'] = {'size': '100%'}
+
 
 
 class DonorForm(FlaskForm):
@@ -19,8 +21,7 @@ class DonorForm(FlaskForm):
     address = StringField('Your Organization Address', [
         InputRequired(), DataRequired()])
     email = StringField('Email', [
-        Email(message=('Not a valid email address.')),
-        InputRequired, DataRequired()])
+        InputRequired(), DataRequired()])
     
     tel = TextField('Your Telephone Number', [
         DataRequired(), InputRequired(),
@@ -28,20 +29,23 @@ class DonorForm(FlaskForm):
     supplyType = StringField('Type of Supply', [InputRequired(), DataRequired()])
     supplyNumber = StringField('Number of Supply', [InputRequired(), DataRequired()])
 
-    recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
 
 class SeekerForm(FlaskForm):
     """Contact form."""
-    name = StringField('Name', [
-        DataRequired()])
+    name = StringField('Your Organization Name', [
+        InputRequired(), DataRequired()])
+    address = StringField('Your Organization Address', [
+        InputRequired(), DataRequired()])
     email = StringField('Email', [
-        Email(message=('Not a valid email address.')),
-        DataRequired()])
-    body = TextField('Message', [
-        DataRequired(),
-        Length(min=4, message=('Your message is too short.'))])
-    recaptcha = RecaptchaField()
+        InputRequired(), DataRequired()])
+    
+    tel = TextField('Your Telephone Number', [
+        DataRequired(), InputRequired(),
+        Length(10, message=('Not a valid phone number'))])
+    supplyType = StringField('Type of Supply', [InputRequired(), DataRequired()])
+    supplyNumber = StringField('Number of Supply', [InputRequired(), DataRequired()])
+
     submit = SubmitField('Submit')
 
 
@@ -52,12 +56,12 @@ def index():
     donorForm = DonorForm()
     if form.validate_on_submit():
         return redirect(url_for('success'))
-    locList = returnLocList()
+    # locList = returnLocList()
 
     return render_template('index.html', 
 
-    form=form, locList = locList, )
+    form=donorForm)
 
 
-if __name__ == "__main__":
+if __name__ == "__ui__":
     app.run(debug=True)
